e2511a2953fdb301dba6cfe0e8c3264e3af83d2a
Sergej <luzhins764@gmail.com>
2024-11-19 18:24:26 +0300

    added char and double

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,39 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        /*int a = -10;
-        int b = -2;
-        int result = (a + b) >>> 1;
-        System.out.println(result);*/
 
+        // 1. Byte binary search
         byte[] a = new byte[]{2, 4, 5, 6, 8, 9, 10, 14, 15, 19, 20};
         byte key = 3;
         int result = MyArrays.binarySearch(a, key);
-
         System.out.println("1. Testing byte binary search.\nArray: " + Arrays.toString(a) + "\nLooking for: " + key);
         if (result >= 0) {
             System.out.printf("Element %d was found at index %d", key, result);
         } else {
             System.out.printf("Element %d was not found. Could be insert at index %d", key, -(result + 1));
         }
+
+        //Char binary search
+        char[] charArray = new char[]{'a', 'b', 'd', 'g', 'i', 'k', 'o', 'q', 'z'};
+        char charKey = 'b';
+        result = MyArrays.binarySearch(charArray, charKey);
+        System.out.println("\n\n2. Testing char binary search.\nArray: " + Arrays.toString(charArray) + "\nLooking for: " + charKey);
+        if (result >= 0) {
+            System.out.printf("Element %c was found at index %d", charKey, result);
+        } else {
+            System.out.printf("Element %c was not found. Could be insert at index %d", charKey, -(result + 1));
+        }
+
+        //Double binary search
+        double[] doubleArray = new double[]{1.3d, 3.54d, 5.41d, 5.46d, 5.465d, 7.43d};
+        double doubleKey = 7.430d;
+        result = MyArrays.binarySearch(doubleArray, doubleKey);
+        System.out.println("\n\n3. Testing double binary search.\nArray: " + Arrays.toString(doubleArray) + "\nLooking for: " + doubleKey);
+        if (result >= 0) {
+            System.out.printf("Element %.2f was found at index %d", doubleKey, result);
+        } else {
+            System.out.printf("Element %.2f was not found. Could be insert at index %d", doubleKey, -(result + 1));
+        }
+
     }
 }
--- src/MyArrays.java
+++ src/MyArrays.java
@@ -1,13 +1,13 @@
 public class MyArrays {
-    static int binarySearch(byte[] a, byte key){
+    static int binarySearch(byte[] a, byte key) {
         return binarySearch(a, 0, a.length, key);
     }
 
-    static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key){
+    static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
         int low = fromIndex;
         int high = toIndex - 1;
 
-        while(low <= high){
+        while (low <= high) {
             int mid = (low + high) / 2; //не использовал побитовый оператор для возможности работы с отрицатльными числами
             byte midVal = a[mid];
 
@@ -19,6 +19,64 @@ public class MyArrays {
                 return mid; //key found
         }
         return -(low + 1); //key not found
+    }
+
+    static int binarySearch(char[] a, char key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            char midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid; // key found
+        }
+        return -(low + 1);  // key not found.
+    }
+
+    static int binarySearch(double[] a, double key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    static int binarySearch(double[] a, int fromIndex, int toIndex, double key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            double midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else {
+                if (midVal == key)
+                    return mid;             // Key found
+                else if (midVal < key)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -(low + 1);
+    }
+
+    static int binarySearch(float[] a, float key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    static int binarySearch(float[] a, int fromIndex, int toIndex, float key) {
+        return 0;
     }
 
 }
